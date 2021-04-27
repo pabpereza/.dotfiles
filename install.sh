947348a81a0d49ca2e0adfed1be499599caf536a
Pablo Pérez-Aradros Calvo <pabpereza@users.noreply.github.com>
2021-04-27 15:01:42 +0200

    Fixed vundle installer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-if [ -d "~/.vim/bundle/Vundle.vim" ];
+if [ ! -d "~/.vim/bundle/Vundle.vim" ];
 then
 	echo "Hola if"
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
